900d388ab3443b5c4280f778236e43054572a605
Lucas Souza <lucasfs7@gmail.com>
2013-07-25 03:56:46 -0300

    add open work fx

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
   $(document).ready(function() {
     var $window = $(window);
     var $appHeader = $('#app-header');
+    var $work = $('#work');
+
+    var animationTime = 500;
     var headerOriginalTop = $appHeader.position().top;
 
     var updateHeader = _.throttle(function() {
@@ -12,6 +15,30 @@
         $appHeader.removeClass('fixed');
       }
     }, 300);
+
+    $work.find('.work-list .item a').on('click', function(e) {
+      e.preventDefault();
+      var t = e.target;
+
+      while (t.nodeName.toLowerCase() != 'a') {
+        t = t.parentNode;
+      }
+
+      var $t = $(t);
+      var $item = $t.parents('.item');
+      var oldHeight = $t.height();
+
+      $item.addClass('open');
+
+      var updateHeight = _.debounce(function() {
+        $t.animate({
+          height: $window.innerHeight() - 12
+        }, animationTime);
+      }, 300);
+
+      updateHeight();
+      $window.on('resize', updateHeight);
+    });
 
     $window.on('scroll', updateHeader);
   });
